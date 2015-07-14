cd50b1e809fcacaa217541ae0a904f2fd2a6a61f
Peter Krautzberger <p.krautzberger@gmail.com>
2015-07-14 09:05:20 +0200

    rewrite feedmerger to use fs; fix some but not all issues around reading author properties

--- feedmerger.js
+++ feedmerger.js
@@ -1,42 +1,47 @@
-#! /usr/bin/env node
+/* eslint-env node */
 
 var FeedParser = require('feedparser'); // to parse feeds
 // var Feed = require('feed'); // to write feeds (but not necessary to require here because we should get a Feed object from app.js -- which seems wrong)
 var async = require('async');
-var request = require('request'); // TODO let's abstract this so that we can switch it out (e.g., to fs.readFile )
+// var request = require('request'); // TODO let's abstract this so that we can switch it out (e.g., to fs.readFile )
+var fs = require('fs');
 
-exports.feedmerger = function (feedsArray, feedObject, mergedCallback) {
+exports.feedmerger = function(feedsJson, feedObject, mergedCallback) {
+  'use strict';
   // TODO some error handling; checking the arguments etc.
 
-  var getFeed = function (feed, callback) {
-    var req = request(feed);
+  var getFeed = function(feed, callback) {
     var feedparser = new FeedParser();
-    req.on('error', function (error) {
-      console.error(error);
+    var req = fs.createReadStream(feed); // request(feed);
+    req.on('error', function(error) {
+        console.error(error);
     });
-    req.on('response', function (res) {
+    req.on('readable', function() {
       var stream = this;
-      if (res.statusCode != 200) {
-        return this.emit('error', new Error('Bad status code'));
-      }
-      //    stream.pipe(process.stdout); //works
       stream.pipe(feedparser);
     });
-    feedparser.on('error', function (error) {
-      console.error(error);
+    // req.on('response', function(res) {
+    //   var stream = this;
+    //   if (res.statusCode !== 200) {
+    //     return this.emit('error', new Error('Bad status code'));
+    //   }
+    //   //    stream.pipe(process.stdout); //works
+    //   stream.pipe(feedparser);
+    // });
+    feedparser.on('error', function(error) {
+      console.error(feed + ' as a parsing error' + error);
     });
-    feedparser.on('readable', function () {
+    feedparser.on('readable', function() {
       // This is where the action is!
       var stream = this;
       //    var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
       var item;
-      while (item = stream.read()) {
-        var itemTitle = item['rss:title']['#']; // how to make it "or use item['atom:title']['#']" ??
+      while ( (item = stream.read())) {
+        var itemTitle = item.title;
         var itemLink = item.link;
         var itemDescription = item.description;
         var itemDate = item.date;
-        var itemAuthor = item.author;
-//        console.error(typeof itemAuthor);
+        var itemAuthor = [{ name: item.author.toString(), email: '', link: ''}];//TODO handle multiple authors??? npm/feed supports that
         feedObject.addItem({
           title: itemTitle,
           link: itemLink,
@@ -46,22 +51,22 @@ exports.feedmerger = function (feedsArray, feedObject, mergedCallback) {
         });
       }
     });
-    feedparser.on('finish', function () {
+    feedparser.on('finish', function() {
       callback();
     });
   };
 
-//  console.log(feedsArray);
-  async.each(feedsArray, getFeed, function (err) {
+  //  console.log(feedsJson);
+  async.each(feedsJson, getFeed, function(err) {
     if (err) {
       throw err;
     }
-    feedObject.items.sort(function (a, b) { //sort by date for creating pages later
+    feedObject.items.sort(function(a, b) { //sort by date for creating pages later
       return b.date - a.date;
     });
-//   console.log("feedmerger completed!");
-//     console.log(feedObject.render('atom-1.0'));
-   return mergedCallback(null,feedObject);
+    //   console.log("feedmerger completed!");
+    //     console.log(feedObject.render('atom-1.0'));
+    return mergedCallback(null, feedObject);
   });
 };
 
