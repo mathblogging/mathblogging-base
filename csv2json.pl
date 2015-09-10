0f7222ce53a0e27449ba2a367e73cd5ac23e45fb
Samuel Coskey <scoskey@gmail.com>
2015-09-10 15:18:00 -0600

    Added some very silly perl code to convert CSV feed data into our future JSON format

---
+++ csv2json.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+print '{' . "\n";
+print '  "feeds":[' . "\n";
+my $row = <>;
+chomp $row;
+my @row_values = split ( /,/, $row );
+my $feed_url = shift @row_values;
+my $categories_string = '"' . join ( '","', @row_values ) . '"';
+print '    {' . "\n";
+print '      "url": "' . $feed_url . '",' . "\n";
+print '      "category" = [' . $categories_string . ']' . "\n";
+while ( my $row = <> ) {
+    chomp $row;
+    my @row_values = split(/,/, $row);
+    my $feed_url = shift @row_values;
+    my $categories_string = '"' . join ( '","', @row_values ) . '"';
+    print '    },' . "\n";
+    print '    {' . "\n";
+    print '      "url": "' . $feed_url . '",' . "\n";
+    print '      "category" = [' . $categories_string . ']' . "\n";
+}
+
+print '  ]' . "\n";
+print '}' . "\n";
