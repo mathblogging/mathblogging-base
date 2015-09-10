832812f92e88339a83d8f82fce99fc0d9a3d9e61
Samuel Coskey <scoskey@gmail.com>
2015-09-10 16:17:46 -0600

    robustify csv code

--- csv2json.pl
+++ csv2json.pl
@@ -1,22 +1,35 @@
 #!/usr/bin/perl
 
 # USAGE
-# ./csv2json.pl < input.csv > output.json
+# perl csv2json.pl < input.csv > output.json
 
 # INPUT FORMAT
 # any number of lines of the form:
 # feed_url,category_1,category_2,...,category_n
+
+# OUTPUT FORMAT
+# {
+#   "feeds":[
+#     {
+#       "url": url,
+#       "category": ["cat1",...]
+#     }
+#   ]
+# }
 
 print '{' . "\n";
 print '  "feeds":[' . "\n";
 
 my $first_iter = 1;
 while ( my $row = <> ) {
-    chomp $row;
+    $row =~ s/\r?\n$//;   # chomp for all platforms
     my @row_values = split( /,/, $row );
     $feed_url = shift @row_values;
-    $categories_string = '"' . join ( '","', @row_values ) . '"';
-    print ",\n" unless $first_iter;
+    @row_values = grep { $_ ne "" } @row_values;  # remove empties
+    @row_values = map { "\"$_\"" } @row_values;   # quote cat names
+    $categories_string = join ( ',', @row_values );
+
+    print ",\n" unless $first_iter;               # no initial ,
     $first_iter = 0;
     print "    {\n";
     print "      \"url\": \"$feed_url\",\n";
