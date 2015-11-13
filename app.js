0c052c8e7edf3739ff2187d21612be5a1c9f9dc9
Peter Krautzberger <p.krautzberger@gmail.com>
2015-11-13 21:33:19 +0100

    [app] sidebar generation: fix URLs; escape ampersands. Fixes #27

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ createCategories(FeedsJson, categories);
 var sidebar = '<nav id="categories">\n';
 for (var c = 0; c < categories.length; c++){
   var cat = categories[c];
-  sidebar += '<a href="{{ site.baseurl }}/' + cat + '.html">' + cat + '</a>\n';
+  var url = categories[c].toLowerCase().replace(/ |'|&/g, '_');
+  sidebar += '<a href="{{ site.baseurl }}/' + url + '.html">' + cat.replace(/&/g, '&amp;') + '</a>\n';
 }
 sidebar += '</nav>';
 fs.writeFile('./mathblogging.org/_includes/sidebar-secondary.html', sidebar, function(err) {
