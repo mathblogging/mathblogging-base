6d505d47b8413eab0cd1c20f4abbd7e537c7af58
Samuel Coskey <scoskey@gmail.com>
2017-06-08 13:14:52 -0600

    include the current run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+eval `ssh-agent`
+ssh-add $HOME/.ssh/mb_id_rsa
+cd $HOME/code/mathblogging-base && \
+    echo "Clean Old Feeds" && \
+    find feeds -mtime +31 -type f -delete  && \
+    echo "Start FeedFetchTrimmer" && \
+    nodejs run_feedFetchTrimmer.js && \
+    echo "Start app.js" && \
+    nodejs app.js && \
+    echo "Do git commit and push" && \
+    cd $HOME/code/mathblogging-base/mathblogging.org && \
+    git add -u && \
+    git commit -m "`date`" && \
+    git push
