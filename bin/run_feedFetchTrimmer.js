c414068641fb45ce5e6a5bd145d709dc3249587d
Peter Krautzberger <p.krautzberger@gmail.com>
2017-06-12 22:13:48 +0200

    [run_feedFetchTrimmer] refactor with ES6 Part of #54

--- bin/run_feedFetchTrimmer.js
+++ bin/run_feedFetchTrimmer.js
@@ -1,41 +1,22 @@
-#! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const feedFetchTrimmer = require('../lib/feedFetchTrimmer.js');
 
-var fs = require('fs');
- var async = require('async'); // asyncjs for async stuff
-var feedFetchTrimmer = require('../lib/feedFetchTrimmer.js');
-// process.setMaxListeners(0);
-fs.readFile('../data/feeds.json', 'utf8', function(err, data) {
-  'use strict';
-  var q = async.queue(feedFetchTrimmer, 5);
-  var FeedsJson = JSON.parse(data);
-  var urls = [];
-  for (var j = 0; j < FeedsJson.blogs.length; j++) {
-    urls.push(FeedsJson.blogs[j].url);
+const data = fs.readFileSync(path.resolve(__dirname, '../data/feeds.json'), 'utf8');
+const q = async.queue(feedFetchTrimmer, 5);
+const FeedsJson = JSON.parse(data);
+
+let urls = FeedsJson.blogs.map((blog)=>blog.url);
+
+q.push(urls, function (error, warning) {
+  if (error) {
+    console.log(error);
   }
-  // console.log(urls);
-  if (err) {
-    throw err;
+  if (warning) {
+    console.log(warning);
   }
-  // Feeds = JSON.parse(data);
-  // var feed = Feeds.some;
-//  var callback = function(e) {
-//    if (e) {
-//      console.log(e);
-//      return;
-//    }
-//    console.log('something\'s odd, you expected an error but did not get one!');
-//  };
-  q.drain = function() {
-    console.log('all items have been processed');
-  };
-  q.push(urls, function(error, warning) {
-    if (error) {
-      console.log(error);
-    }
-    if (warning) {console.log(warning); }
-    //console.log('finished processing item');
-  });
-//  for (var i = 0; i < urls.length; i++) {
-//      feedFetchTrimmer(urls[i], callback);
-//  }
 });
+q.drain = function () {
+  console.log('all items have been processed');
+};
