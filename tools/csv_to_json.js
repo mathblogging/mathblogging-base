4e545e70638a07d7e4d82c8db635cfc3f1a9dfa9
Peter Krautzberger <p.krautzberger@gmail.com>
2017-06-12 22:22:33 +0200

    [csv_to_json] refactor * adopt ES6 syntax, part of #54 * properly resolve paths, part of #67

--- tools/csv_to_json.js
+++ tools/csv_to_json.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
-fs.readFile('./feeds.csv', 'utf8', function(err, data) {
-  if (err) {
-    throw err;
-  }
-  const lines = data.split('\n');
-  const feedsJson = {
-    blogs: []
+const path = require('path');
+
+const csv = fs.readFileSync(path.resolve(__dirname, '../data/feeds.csv'), 'utf8');
+const lines = csv.split('\n');
+const feedsJson = {
+  blogs: []
+};
+
+for (let line of lines) {
+  var cells = line.split(',');
+  var entry = {
+    url: cells[0],
+    categories: []
   };
-  for (let line of lines) {
-    var cells = line.split(',');
-    var entry = {
-      url: cells[0],
-      categories: []
-    };
-    for (let c = 1; c < 4; c++){
-      const category = cells[c].trim();
-      if (category !== "" && entry.categories.indexOf(category) < 0) entry.categories.push(category);
-    }
-    feedsJson.blogs.push(entry);
+  for (let c = 1; c < 4; c++) {
+    const category = cells[c].trim();
+    if (category !== "" && entry.categories.indexOf(category) < 0) entry.categories.push(category);
   }
-  fs.writeFile('./feeds.json', JSON.stringify(feedsJson, null, 4), function(e) {
-    if (e) {
-      throw e;
-    }
-  });
+  feedsJson.blogs.push(entry);
+}
+
+fs.writeFile(path.resolve(__dirname, '../data/feeds.json'), JSON.stringify(feedsJson, null, 4), function (e) {
+  if (e) {
+    throw e;
+  }
 });
