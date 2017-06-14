ef8b8209bca4ad565f75b5284b229e3a2b687490
Peter Krautzberger <p.krautzberger@gmail.com>
2017-06-14 21:44:39 +0200

    [csv_to_json] fetch csv from google sheets and process Resolves #51

--- tools/csv_to_json.js
+++ tools/csv_to_json.js
@@ -1,27 +1,42 @@
 const fs = require('fs');
 const path = require('path');
+const got = require('got');
 
-const csv = fs.readFileSync(path.resolve(__dirname, '../data/feeds.csv'), 'utf8');
-const lines = csv.split('\n');
-const feedsJson = {
-  blogs: []
-};
+const csvUrl = 'https://docs.google.com/spreadsheets/d/1tDmRWouGtsN1VGg1_77Z8hnb7FeHkppqbGCfSsDq9wk/pub?gid=1891944288&single=true&output=csv';
 
-for (let line of lines) {
-  var cells = line.split(',');
-  var entry = {
-    url: cells[0],
-    categories: []
+const csv2json = function (csv) {
+  const lines = csv.split('\n');
+  const feedsJson = {
+    blogs: []
   };
-  for (let c = 1; c < 4; c++) {
-    const category = cells[c].trim();
-    if (category !== "" && entry.categories.indexOf(category) < 0) entry.categories.push(category);
+
+  for (let line of lines) {
+    var cells = line.split(',');
+    var entry = {
+      url: cells[0],
+      categories: []
+    };
+    for (let c = 1; c < 4; c++) {
+      const category = cells[c].trim();
+      if (category !== "" && entry.categories.indexOf(category) < 0) entry.categories.push(category);
+    }
+    feedsJson.blogs.push(entry);
   }
-  feedsJson.blogs.push(entry);
+
+  fs.writeFile(path.resolve(__dirname, '../data/feeds.json'), JSON.stringify(feedsJson, null, 4), function (e) {
+    if (e) {
+      throw e;
+    }
+  });
 }
 
-fs.writeFile(path.resolve(__dirname, '../data/feeds.json'), JSON.stringify(feedsJson, null, 4), function (e) {
-  if (e) {
-    throw e;
-  }
-});
+got(csvUrl)
+  .then(response => {
+    csv2json(response.body)
+    //=> '<!doctype html> ...'
+  })
+  .catch(error => {
+    console.log(error.response.body);
+    //=> 'Internal server error ...'
+  });
+
