51f83dfbab315ed112569948b2282f35ba134207
Peter Krautzberger <p.krautzberger@gmail.com>
2017-08-16 22:17:19 +0200

    [wgetter] add quiet option Resolves #91

--- tools/wgetter.sh
+++ tools/wgetter.sh
@@ -3,6 +3,6 @@
 n=0;
 while read p; do
 # retry magic from https://superuser.com/a/689340
-  wget --retry-connrefused --waitretry=1 --read-timeout=3 --timeout=3 -t 3   $p -O ./feeds/$n.xml
+  wget --quiet --retry-connrefused --waitretry=1 --read-timeout=3 --timeout=3 -t 3   $p -O ./feeds/$n.xml
   n=$((n+1))
 done <./data/wgetList.txt
