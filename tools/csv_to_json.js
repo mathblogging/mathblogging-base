95e55b4ba8f96b6df64c74208906d41fffa87b4b
Peter Krautzberger <p.krautzberger@gmail.com>
2020-08-22 14:32:14 +0200

    feat!: rewrite csv_to_json to read from file
    Cf. mathblogging/mathblogging.org#32

--- tools/csv_to_json.js
+++ tools/csv_to_json.js
@@ -3,6 +3,7 @@ const path = require('path');
 const https = require('https');
 
 const csv2json = function (csv) {
+  // console.log(csv);
   const lines = csv.split('\n');
   const feedsJson = {
     blogs: []
@@ -18,7 +19,8 @@ const csv2json = function (csv) {
     };
     wgetList += entry.url + '\n';
     for (let c = 1; c < 4; c++) {
-      const category = cells[c].trim();
+     // if (!cells[c]) continue;
+	  const category = cells[c].trim();
       if (category !== "" && entry.categories.indexOf(category) < 0) entry.categories.push(category);
     }
     feedsJson.blogs.push(entry);
@@ -37,16 +39,21 @@ const csv2json = function (csv) {
 }
 
 // google sheets URL to public sheet (as CSV)
-const csvUrl = 'https://docs.google.com/spreadsheets/d/1tDmRWouGtsN1VGg1_77Z8hnb7FeHkppqbGCfSsDq9wk/pub?gid=1891944288&single=true&output=csv';
+// const csvUrl = 'https://docs.google.com/spreadsheets/d/1tDmRWouGtsN1VGg1_77Z8hnb7FeHkppqbGCfSsDq9wk/pub?gid=1891944288&single=true&output=csv';
 
-const stream = https.get(csvUrl, (res) => {
-  const chunks = [];
-  res.on('data', (chunk) => {
-    chunks.push(chunk);
-  });
-  res.on('end', () => {
-    csv2json(chunks.join(''));
-  })
-}).on('error', (error) => {
-  throw error;
-});
+const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTohVihVKYUFb7REtchf6PcoMejdrgEj4oTAphUhSUeREOau9QU8n1wGKg9cWNSmxMhMP1D3WKeyiw9/pub?gid=1891944288&single=true&output=csv';
+
+//const stream = https.get(csvUrl, (res) => {
+//  const chunks = [];
+//  res.on('data', (chunk) => {
+//    chunks.push(chunk);
+//  });
+//  res.on('end', () => {
+//    csv2json(chunks.join(''));
+//  })
+//}).on('error', (error) => {
+//  throw error;
+//});
+
+const csvData = fs.readFileSync('data.csv').toString();
+csv2json(csvData);
