305d029b842850e71ec761e228e93f98ee23c02f
Peter Krautzberger <p.krautzberger@gmail.com>
2020-08-24 21:25:51 +0200

    feat: adjust for CI usage
    * add ci.sh for github action
    * app.js:  accept CLI paramater for target directory
      * and pass it along to components
    * publishCategory, speciaPages: accept directory and use it
    * editors: accept directory, adjust API key access
    
    Resolves #103

---
+++ bin/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Fetch csv"
+wget -O data.csv "https://docs.google.com/spreadsheets/d/e/2PACX-1vTohVihVKYUFb7REtchf6PcoMejdrgEj4oTAphUhSUeREOau9QU8n1wGKg9cWNSmxMhMP1D3WKeyiw9/pub?gid=1891944288&single=true&output=csv"
+echo "Generate JSON"
+node ./tools/csv_to_json.js
+echo "Download feeds"
+sh ./tools/wgetter.sh
+echo "Generate entries.json"
+node ./lib/generateEntries.js
+echo "Run app.js"
+node ./lib/app.js $1
