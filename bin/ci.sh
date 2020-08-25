f99a7be21279fb98f8333329d6609385255f3f74
Peter Krautzberger <p.krautzberger@gmail.com>
2020-08-25 10:16:13 +0200

    fix(ci.sh): create ./feeds
    Avoids wget failure on CI.
    
    Fixes #105

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,6 +5,7 @@ wget -O data.csv "https://docs.google.com/spreadsheets/d/e/2PACX-1vTohVihVKYUFb7
 echo "Generate JSON"
 node ./tools/csv_to_json.js
 echo "Download feeds"
+mkdir -p ./feeds
 sh ./tools/wgetter.sh
 echo "Generate entries.json"
 node ./lib/generateEntries.js
